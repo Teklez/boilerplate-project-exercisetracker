a271e57fb87a98035437350c1e8102394109b7c4
Teklez <zemenumekiria@gmail.com>
2024-10-20 21:23:07 +0300

    bug fixed

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ app.post("/api/users", async (req, res) => {
 
   try {
     const userExists = await userModel.findOne({ username: username });
-    if (userExits) {
+    if (userExists) {
       throw { message: "User already exists!" };
     } else {
       const newUser = new userModel({ username: username });
-      newUser.save();
+      await newUser.save();
       res.json({
         username: newUser.username,
         _id: newUser._id,
@@ -49,62 +49,81 @@ app.post("/api/users", async (req, res) => {
 
 app.post("/api/users/:_id/exercises", async (req, res) => {
   const { duration, description, date } = req.body;
-  const id = req.body[":_id"];
-  const objectId = new mongoose.Types.ObjectId(id);
-  console.log(objectId);
+  const id = req.params._id;
+  const dateObj = date ? new Date(date) : new Date();
+
   try {
-    const user = await userModel.findById(objectId);
-    const dateObj = new Date(date);
+    const user = await userModel.findById(id);
     if (user) {
       user.log.push({
         description: description,
-        duration: duration,
-        date: dateObj,
+        duration: parseInt(duration),
+        date: dateObj.toDateString(),
       });
-      user.save();
-      
+      await user.save();
 
       res.json({
         username: user.username,
-        _id: user._id,
         description: description,
-        duration: duration,
+        duration: parseInt(duration),
         date: dateObj.toDateString(),
+        _id: user._id,
       });
     } else {
-      console.log("user not found");
+      console.log("user not found", id);
     }
   } catch (err) {
     console.log(err);
   }
 });
 
+app.get("/api/users", async (req, res) => {
+  const users = await userModel.find().select("username _id").exec();
+  res.send(users);
+});
+
 app.get("/api/users/:_id/logs", async (req, res) => {
   const _id = req.params._id;
   const obId = new mongoose.Types.ObjectId(_id);
+
+  const { from, to, limit } = req.query;
+
   try {
     const user = await userModel.findById(obId);
     if (!user) {
       throw { message: "User not found" };
-    } else {
-      let logsWithoutId = [];
-      for (log of user.log) {
-        logsWithoutId.push({
-          description: log.description,
-          duration: log.duration,
-          date: log.date,
-        });
-      }
-
-      res.json({
-        username: user.username,
-        count: user.log.length,
-        _id: user._id,
-        logs: logsWithoutId,
-      });
     }
+
+    let logs = user.log;
+
+    if (from) {
+      const fromDate = new Date(from);
+      logs = logs.filter((log) => new Date(log.date) >= fromDate);
+    }
+    if (to) {
+      const toDate = new Date(to);
+      logs = logs.filter((log) => new Date(log.date) <= toDate);
+    }
+
+    if (limit) {
+      logs = logs.slice(0, parseInt(limit));
+    }
+
+    const logsWithoutId = logs.map((log) => ({
+      description: log.description.toString(),
+      duration: parseInt(log.duration),
+      date: log.date.toString(),
+    }));
+
+    res.json({
+      username: user.username,
+      count: parseInt(logs.length),
+      _id: user._id,
+      log: logsWithoutId,
+    });
   } catch (err) {
     console.log(err);
+    res.status(500).json({ error: "An error occurred" });
   }
 });
 
